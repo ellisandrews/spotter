13216813a0f878f577fa9fdee9bb9a4412e89fe1
ellisandrews <ellisandrews1@gmail.com>
2020-03-24 11:45:49 -0500

    Starting workout routes

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,19 @@
+class WorkoutsController < ApplicationController
+
+    def index
+        @workouts = Workout.all
+    end
+
+    def show
+        @workout = Workout.find(params[:id])
+    end
+
+    def new
+        @workout = Workout.new
+    end
+
+    def create
+        p 'TODO!'
+    end
+
+end
