90470c8362f58aba12b315a6b64c5348caa4d66c
ellisandrews <ellisandrews1@gmail.com>
2020-03-25 18:45:24 -0500

    Progress on scheduled workouts

--- app/models/scheduled_workout.rb
+++ app/models/scheduled_workout.rb
@@ -1,4 +1,24 @@
 class ScheduledWorkout < ApplicationRecord
+  # Associations
   belongs_to :workout
   has_one :user, through: :workout
+
+  # Validations
+  # TODO!
+
+  def pretty_date
+    date.strftime('%B %-d, %Y (%A)')  # March 25, 2020 (Wednesday)
+  end
+
+  def pretty_completed
+    case completed
+    when true
+      'Yes'
+    when false
+      'No'
+    else
+      'N/A'
+    end
+  end
+
 end
