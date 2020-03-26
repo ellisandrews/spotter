582109ce69b51f066e65035474a9cd23c6ee866f
ellisandrews <ellisandrews1@gmail.com>
2020-03-26 10:38:51 -0500

    Add Muscles views

--- app/controllers/muscles_controller.rb
+++ app/controllers/muscles_controller.rb
@@ -1,9 +1,11 @@
 class MusclesController < ApplicationController
 
     def index
+        @muscles = Muscle.order(:name)
     end
 
     def show
+        @muscle = Muscle.find(params[:id])
     end
 
 end
