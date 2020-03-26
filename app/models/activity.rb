01fb12748fd79c0cfe09c7d6030c07b63a145012
ellisandrews <ellisandrews1@gmail.com>
2020-03-25 19:42:22 -0500

    Touch up some views and model validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,10 @@
 class Activity < ApplicationRecord
+  # Associations
   belongs_to :exercise
   belongs_to :workout
+
+  # Validations
+  validates :sets, presence: true
+  validates :reps, presence: true
+  validates :weight, presence: true
 end
