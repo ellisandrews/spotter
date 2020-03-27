ece5211f8a07bec99b6d78746b62ccec28b47247
ellisandrews <ellisandrews1@gmail.com>
2020-03-27 13:59:34 -0500

    Add some basic analytics

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,18 +24,18 @@ class User < ApplicationRecord
     end
     
     def favorite_exercise
-        query = %q(
+        query = %{
             SELECT e.id, 
                    COUNT(*) AS records
             FROM scheduled_workouts sw
             JOIN workouts w ON w.id = sw.workout_id 
             JOIN activities a ON a.workout_id = w.id
             JOIN exercises e ON e.id = a.exercise_id
-            WHERE w.user_id = 1
+            WHERE w.user_id = #{self.id}
             GROUP BY 1
             ORDER BY 2 DESC
             LIMIT 1;
-        )
+        }     
         results = ActiveRecord::Base.connection.execute(query)
         if results.any?
             Exercise.find(results.first['id'])
@@ -43,6 +43,27 @@ class User < ApplicationRecord
     end
 
     def favorite_muscle
-
+        query = %{
+            SELECT m.id, 
+                   COUNT(*) AS count
+            FROM scheduled_workouts sw
+            JOIN workouts w ON w.id = sw.workout_id 
+            JOIN activities a ON a.workout_id = w.id
+            JOIN exercises e ON e.id = a.exercise_id
+            JOIN muscle_exercises me ON me.exercise_id = e.id
+            JOIN muscles m ON m.id = me.muscle_id
+            WHERE w.user_id = #{self.id}
+            GROUP BY 1
+            ORDER BY 2 DESC
+            LIMIT 1;
+        }
+        results = ActiveRecord::Base.connection.execute(query)
+        if results.any?
+            Muscle.find(results.first['id'])
+        end
     end
+
+    # def workout_completion_rate
+    #     ScheduledWorkout.where(user: self, completed: true, date: Date.new..Date.today).count.to_f / ScheduledWorkout.where(user: self, date: Date.new..Date.today).count
+    # end
 end
